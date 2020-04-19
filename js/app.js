8c58fb2b90babd3ba7e0b90287760aa54ccdf5ea
Xeron081495 <germang08@hotmail.com>
2020-04-18 22:47:15 -0300

    Detectar teclas para mover
    Agrega la opción de usar las teclas para mover las celdas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
 //creo la celda
 const grilla = new Grid(4);
 
+// verificar si apretas una tecla
+document.onkeydown = checkKey;
+
+
+
+
+function checkKey(e) {
+    e = e || window.event;
+    if (e.keyCode == '38') {
+        moverAbajo();
+    }else if (e.keyCode == '40') {
+        moverAbajo();
+    }else if (e.keyCode == '37') {
+        moverIzquierda();
+    }else if (e.keyCode == '39') {
+        moverDerecha();
+    }
+
+}
+
 function moverDerecha(){
     grilla.mover(new MovimientoDerecha(grilla));
 }
