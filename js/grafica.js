e731990ceae462fe18036dc90a6f64782e8ef866
Xeron081495 <germang08@hotmail.com>
2020-04-24 20:38:54 -0300

    [Fix] efecto fade In al agregar celda

--- js/grafica.js
+++ js/grafica.js
@@ -10,41 +10,50 @@ class Grafica{
     actualizarPuntaje(puntaje){
         document.getElementById('puntaje-actual').innerText = puntaje;
     }
+
     actualizarRecord(puntaje){
         document.getElementById('puntaje-record').innerText = puntaje;
     }
+
+    // elimina una celda
     deleteCelda(x,y){
-        const idCelda = 'cell-'+x+'-'+y+'';
-        document.getElementById(idCelda).innerText = '';
+        let celda = this.getCelda(x,y);
+        celda.innerText = '';
         for(let valor of this.valores)
-            $('#'+idCelda).removeClass('type-'+valor);
-        $('#'+idCelda).removeClass('animated fadeIn');
-    }
-    setCelda(valor,x,y){
-        const idCelda = 'cell-'+x+'-'+y+'';
-        this.deleteCelda(x,y);
-        document.getElementById(idCelda).innerText = valor;
-        $('#'+idCelda).addClass('type-'+valor+'');
+            celda.classList.remove('type-'+valor);
     }
 
+    // setea una celda en la pos (x,y) cun un valor
+    setCelda(valor,x,y){
+        let celda = this.getCelda(x,y);
+        this.deleteCelda(x,y);
+        celda.innerText = valor;
+        celda.classList.add('type-'+valor+'');
+    }
+
+    //setea una celda con el efecto face in
     setCeldaEfecto(valor,x,y){
         this.setCelda(valor,x,y);
-        const idCelda = 'cell-'+x+'-'+y+'';
-        $('#'+idCelda).addClass('animated fadeIn');
+        let celda = this.getCelda(x,y);
+        this.setEfecto(celda,'fadeIn');
     }
 
-    /*efectoColision(x,y){
-        const idCelda = 'cell-'+x+'-'+y+'';
-        $('#'+idCelda).removeClass('animated');
-        $('#'+idCelda).removeClass('animated shake');
-        $('#'+idCelda).addClass('animated shake');
-    }*/
-
+    // genera el efecto de colision en una celda
     efectoColision(x,y){
-        const celda = document.querySelector('#cell-'+x+'-'+y+'');
-        celda.classList.add('animated', 'shake');
+        let celda = this.getCelda(x,y);
+        this.setEfecto(celda,'shake');
+    }
+
+    // Retorna el div de la celda en x:y
+    getCelda(x,y){
+        return document.querySelector('#cell-'+x+'-'+y+'');
+    }
+
+    // agrega el efecto de animate css y 300ms lo saca para que pueda ser usado nuevamente 
+    setEfecto(celda,efecto){        
+        celda.classList.add('animated', efecto);
         setTimeout(function (){
-            celda.classList.remove('animated', 'shake');
+            celda.classList.remove('animated', efecto);
         },300);
     }
 
