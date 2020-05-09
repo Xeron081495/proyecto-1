4f00e557bbd013c0853db616031fd4b0bf257c5c
Xeron081495 <germang08@hotmail.com>
2020-05-09 14:12:04 -0300

    agregar info modo claro

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ setDirecciones();
 
 
 
-/*grilla.setCelda(2,0,0);
+grilla.setCelda(2,0,0);
 grilla.setCelda(4,0,1);
 grilla.setCelda(8,0,2);
 grilla.setCelda(16,0,3);
@@ -25,7 +25,7 @@ grilla.setCelda(64,1,2);
 grilla.setCelda(128,1,1);
 grilla.setCelda(256,1,0);
 grilla.setCelda(512,2,0);
-grilla.setCelda(1024,2,1);*/
+grilla.setCelda(1024,2,1);
 
 // las proximas 4 funciones indican que movimientos hacer a la grilla (puede venir desde la parte tactil, flechas de teclado o swap en el celu)
 function moverDerecha(){
